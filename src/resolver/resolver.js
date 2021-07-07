b2b29e3485b69961c09ae19191ad763de4dd3ec2
VanDu <nguyenvandu199740@gmail.com>
2021-07-07 21:50:01 +0700

    update code

--- src/resolver/resolver.js
+++ src/resolver/resolver.js
@@ -1,17 +1,59 @@
+const books = [
+  {
+    id: 1,
+    name: "The name of me",
+    generated: "Advanture",
+    authorId: 1,
+  },
+  {
+    id: 2,
+    name: "The name of me two",
+    generated: "Education",
+    authorId: 2,
+  },
+  {
+    id: 3,
+    name: "The name of me",
+    generated: "Advanture",
+    authorId: 3,
+  },
+  {
+    id: 4,
+    name: "The name of me two",
+    generated: "Education",
+    authorId: 4,
+  },
+];
+const authors = [
+  {
+    id: 1,
+    name: "John Cena",
+    age: 40,
+  },
+  {
+    id: 2,
+    name: "Justin bibber",
+    age: 34,
+  },
+  {
+    id: 3,
+    name: "Taylor Swift",
+    age: 29,
+  },
+  {
+    id: 4,
+    name: "Logan Paul",
+    age: 25,
+  },
+];
 const resolver = {
   Query: {
-    books: () => [
-      {
-        id: 1,
-        name: "The name of me",
-        generated: "Advanture",
-      },
-      {
-        id: 2,
-        name: "The name of me two",
-        generated: "Education",
-      },
-    ],
+    books: () => books,
+    authors: () => authors,
+    book: (parent, args) =>
+      books.find((book) => book.id.toString() === args.id),
+    author: (parent, args) =>
+      authors.find((author) => author.id.toString() === args.id),
   },
 };
 
--- src/schema/schema.js
+++ src/schema/schema.js
@@ -4,10 +4,19 @@ const typeDefs = gql`
     id: ID
     name: String
     generated: String
+    author: Author
+  }
+  type Author {
+    id: ID!
+    name: String
+    age: Int
   }
   #root type
   type Query {
     books: [Book]
+    book(id: ID!): Book
+    authors: [Author]
+    author(id: ID!): Author
   }
 `;
 module.exports = typeDefs;
